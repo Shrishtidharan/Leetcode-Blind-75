03a2866f406a4280ff955fb8ca232a24299f3cb7
Shrishtidharan K A <68120150+Shrishtidharan@users.noreply.github.com>
2023-01-22 22:03:43 +0530

    Update MostFreuqnetLetter.py

--- MostFreuqnetLetter.py
+++ MostFreuqnetLetter.py
@@ -11,3 +11,22 @@ def findmostfreq(sentence):
         
 sentence = list(input().split())
 findmostfreq(sentence)
+
+
+or 
+
+#
+def mostfreq(word):
+    ltrcount={}
+    for ltr in word:
+        ltrcount[ltr] = 1+ ltrcount.get(ltr,0)
+    maxi = max(ltrcount.values())
+    for key , value in ltrcount.items():
+        if value == maxi:
+            return key
+def findmostfreq(sentence):
+    for word in sentence:
+        print(mostfreq(word))
+        
+sentence = list(input().split())
+findmostfreq(sentence)
